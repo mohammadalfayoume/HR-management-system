60526e712a210e372357b73be5040e05294d4c90
Mohammad Alfayoume <mabed3819@gmail.com>
2022-08-06 18:02:01 +0300

    v 04

--- app.js
+++ app.js
@@ -3,78 +3,6 @@ var allEmp = [];
 var id = 1000;
 
 
-var container =document.createElement('div');
-container.className='container';
-
-var body=document.getElementsByTagName('body')
-body[0].appendChild(container);
-
-// var container =document.createElement('div');
-// container.className='container';
-
-// var body=document.getElementsByTagName('body')
-// body[0].appendChild(container);
-
-// let numOfEmployees =7;
-// let numOfEmployeeInEachRow =3;
-// let numberOfRows=Math.ceil(numOfEmployees/numOfEmployeeInEachRow)
-
-// var row=[];
-// var employee=[]
-// for (let i=0; i<numberOfRows ;i++){
-// row[i]=document.createElement('div');
-// row[i].className='row ' +(i+1)
-// container.appendChild(row[i]);
-
-// //Row style
-// row[i].style.display='flex'
-// row[i].style.justifyContent='space-between'
-// row[i].style.gap='50px'
-
-// for (let j=0; j<numOfEmployeeInEachRow;j++){
-
-//   if (row[i]==row[numberOfRows-1]){
-//     employee[j]= document.createElement('div');
-//     employee[j].className='employee '+(j+1);
-//     row[i].appendChild(employee[j]);
-//     // employee[j].style.backgroundColor='green'
-//     let picture=document.createElement('img')
-//     picture.style.width='250px';
-//     let paragraph_01= document.createElement('p')
-//     paragraph_01.className='p1';
-//     let paragraph_02= document.createElement('p')
-//     paragraph_02.className='p2';
-//     let paragraph_03= document.createElement('p')
-//     paragraph_03.className='p3';
-//     employee[j].append(picture,paragraph_01,paragraph_02,paragraph_03)
-//     break;
-//   }
-
-//   employee[j]= document.createElement('div');
-//   employee[j].className='employee '+(j+1);
-//   row[i].appendChild(employee[j]);
-//   // employee[j].style.backgroundColor='green'
-//   let picture=document.createElement('img')
-//   picture.src=
-//   picture.style.width='250px';
-//   let paragraph_01= document.createElement('p')
-//   paragraph_01.className='p1';
-//   // paragraph_01.textContent=`ID: ${employee_01.employeeId}`
-//   let paragraph_02= document.createElement('p')
-//   paragraph_02.className='p2';
-//   let paragraph_03= document.createElement('p')
-//   paragraph_03.className='p3';
-
-//   employee[j].append(picture,paragraph_01,paragraph_02,paragraph_03)
-  
-// }
-
-// }
-
-// container.style.display='flex';
-// container.style.flexDirection='column';
-// container.style.gap='50px';
-
 
 
 
@@ -102,79 +30,6 @@ function Employee(fullName, department, level, image, salary) {
   this.salary = salary;
   allEmp.push(this);
 }
-
-// var container =document.createElement('div');
-// container.className='container';
-
-// var body=document.getElementsByTagName('body')
-// body[0].appendChild(container);
-
-// let numOfEmployees =7;
-// let numOfEmployeeInEachRow =3;
-// let numberOfRows=Math.ceil(numOfEmployees/numOfEmployeeInEachRow)
-
-// var row=[];
-// var employee=[]
-// for (let i=0; i<numberOfRows ;i++){
-// row[i]=document.createElement('div');
-// row[i].className='row ' +(i+1)
-// container.appendChild(row[i]);
-
-// //Row style
-// row[i].style.display='flex'
-// row[i].style.justifyContent='space-between'
-// row[i].style.gap='50px'
-
-// for (let j=0; j<numOfEmployeeInEachRow;j++){
-
-//   if (row[i]==row[numberOfRows-1]){
-//     employee[j]= document.createElement('div');
-//     employee[j].className='employee '+(j+1);
-//     row[i].appendChild(employee[j]);
-//     // employee[j].style.backgroundColor='green'
-//     let picture=document.createElement('img')
-//     picture.style.width='250px';
-//     let paragraph_01= document.createElement('p')
-//     paragraph_01.className='p1';
-//     let paragraph_02= document.createElement('p')
-//     paragraph_02.className='p2';
-//     let paragraph_03= document.createElement('p')
-//     paragraph_03.className='p3';
-//     employee[j].append(picture,paragraph_01,paragraph_02,paragraph_03)
-//     break;
-//   }
-
-//   employee[j]= document.createElement('div');
-//   employee[j].className='employee '+(j+1);
-//   row[i].appendChild(employee[j]);
-//   // employee[j].style.backgroundColor='green'
-//   let picture=document.createElement('img')
-//   picture.src=`${employee_01.image}`
-//   picture.src=`${employee_02.image}`
-//   picture.style.width='250px';
-//   let paragraph_01= document.createElement('p')
-//   paragraph_01.className='p1';
-//   // paragraph_01.textContent=`ID: ${employee_01.employeeId}`
-//   let paragraph_02= document.createElement('p')
-//   paragraph_02.className='p2';
-//   let paragraph_03= document.createElement('p')
-//   paragraph_03.className='p3';
-
-//   employee[j].append(picture,paragraph_01,paragraph_02,paragraph_03)
-  
-// }
-
-// }
-
-// container.style.display='flex';
-// container.style.flexDirection='column';
-// container.style.gap='50px';
-
-// Employee.prototype.render = function () {
-
-  
-
-// };
 
 
 const employee_01 = new Employee(
@@ -227,135 +82,125 @@ const employee_07 = new Employee(
   salary(1000, 1500)
 );
 
-// var container =document.createElement('div');
-// container.className='container';
+var container =document.createElement('div');
+container.className='container';
 
-// var body=document.getElementsByTagName('body')
-// body[0].appendChild(container);
+var main=document.getElementsByTagName('main')
+main[0].appendChild(container);
 
-let numOfEmployees =7;
-let numOfEmployeeInEachRow =3;
-let numberOfRows=Math.ceil(numOfEmployees/numOfEmployeeInEachRow)
-
-var row=[];
-var employee=[]
-for (let i=0; i<numberOfRows ;i++){
-row[i]=document.createElement('div');
-row[i].className='row ' +(i+1)
-container.appendChild(row[i]);
-
-//Row style
-row[i].style.display='flex'
-row[i].style.justifyContent='space-between'
-row[i].style.gap='50px'
-
-for (let j=0; j<numOfEmployeeInEachRow;j++){
-
-  if (row[i]==row[numberOfRows-1]){
-    employee[j]= document.createElement('div');
-    employee[j].className='employee '+(j+1);
-    row[i].appendChild(employee[j]);
-    // employee[j].style.backgroundColor='green'
-    let picture=document.createElement('img')
-    picture.style.width='250px';
-    let paragraph_01= document.createElement('p')
-    paragraph_01.className='p1';
-    let paragraph_02= document.createElement('p')
-    paragraph_02.className='p2';
-    let paragraph_03= document.createElement('p')
-    paragraph_03.className='p3';
-    employee[j].append(picture,paragraph_01,paragraph_02,paragraph_03)
-    break;
-  }
-
-  employee[j]= document.createElement('div');
-  employee[j].className='employee '+(j+1);
-  row[i].appendChild(employee[j]);
-  // employee[j].style.backgroundColor='green'
-  let picture=document.createElement('img')
-  picture.style.width='250px';
-  let paragraph_01= document.createElement('p')
-  paragraph_01.className='p1';
-  // paragraph_01.textContent=`ID: ${employee_01.employeeId}`
-  let paragraph_02= document.createElement('p')
-  paragraph_02.className='p2';
-  let paragraph_03= document.createElement('p')
-  paragraph_03.className='p3';
-
-  employee[j].append(picture,paragraph_01,paragraph_02,paragraph_03)
-
-}
-
-}
-
-container.style.display='flex';
-container.style.flexDirection='column';
-container.style.gap='50px';
-
-Employee.prototype.render = function () {
-
-  
-
-};
-
-
-// var row=[];
-// var employee=[]
-// for (let i=0; i<numberOfRows ;i++){
-// row[i]=document.createElement('div');
-// row[i].className='row ' +(i+1)
-// container.appendChild(row[i]);
-// row[i].style.display='flex'
-// row[i].style.justifyContent='space-between'
-// row[i].style.gap='50px'
-// for (let j=0; j<numOfEmployeeInEachRow;j++){
-//   if (row[i]==row[numOfEmployeeInEachRow-1]){
-//     employee[j]= document.createElement('div');
-//     employee[j].className='employee '+(j+1);
-//     row[i].appendChild(employee[j]);
-//     employee[j].style.backgroundColor='green'
-//     let picture=document.createElement('img')
-//     employee[j].appendChild(picture);
-//     picture.style.width='250px';
-//     let paragraph_01= document.createElement('p')
-//     paragraph_01.className='p1';
-//     paragraph_01.textContent=`Hello: ${employee[j].employeeId}`
-//     employee[j].appendChild(paragraph_01);
-//     let paragraph_02= document.createElement('p')
-//     paragraph_02.className='p2';
-//     employee[j].appendChild(paragraph_02);
-//     let paragraph_03= document.createElement('p')
-//     paragraph_03.className='p3';
-//     employee[j].appendChild(paragraph_03);
-//     break;
-//   }
-//   employee[j]= document.createElement('div');
-//   employee[j].className='employee '+(j+1);
-//   row[i].appendChild(employee[j]);
-//   employee[j].style.backgroundColor='green'
-//   let picture=document.createElement('img')
-//   employee[j].appendChild(picture);
-//   picture.style.width='250px';
-//   let paragraph_01= document.createElement('p')
-//   paragraph_01.className='p1';
-//   employee[j].appendChild(paragraph_01);
-//   let paragraph_02= document.createElement('p')
-//   paragraph_02.className='p2';
-//   employee[j].appendChild(paragraph_02);
-//   let paragraph_03= document.createElement('p')
-//   paragraph_03.className='p3';
-//   employee[j].appendChild(paragraph_03);
-  
-// }
-// }
+container.className='container'
 
 // container.style.display='flex';
 // container.style.flexDirection='column';
 // container.style.gap='50px';
 
 
+let numOfEmployees =7;
+let numOfEmployeeInEachRow =3;
+let numberOfRows=Math.ceil(numOfEmployees/numOfEmployeeInEachRow)
 
-// console.log(container)
+var rows=[];
+
+for (let i=0; i<numberOfRows ;i++){
+
+let row=document.createElement('div');
+row.className='row'
+rows.push(row)
+container.appendChild(row);
+}
+let counter=0;
+for (let i = 0; i <rows.length; i++) {
+  for (let j = 0; j <numOfEmployeeInEachRow; j++) {
+    let card=document.createElement('div');
+    card.className='emp'
+
+    
+
+    let employee=allEmp[counter]
+    if (employee==undefined){
+      break;
+    }
+
+    let picture=document.createElement('img');
+    picture.src=employee['image']
+    picture.width=250
+
+    let name= employee['fullName']
+    let emploId= employee.employeeId()
+    let p1=document.createElement('p');
+    p1.textContent='Name: '+name+' - ID: '+emploId
+
+    let depart =employee['department']
+    let lvl =employee['level']
+    let p2=document.createElement('p')
+    p2.textContent='Department: '+depart+' - Level: '+lvl
+
+    let sal=employee['salary']
+    let p3=document.createElement('p')
+    p3.textContent=sal
+
+
+    card.append(picture,p1,p2,p3)
+
+
+    // if (employee==undefined){
+    //   break;
+    // }
+    rows[i].appendChild(card);
+
+  counter++
+
+  }
+}
+//Row style
+// row[i].style.display='flex'
+// row[i].style.justifyContent='space-between'
+// row[i].style.gap='50px'
+
+//   for (let j=0; j<numOfEmployeeInEachRow;j++){
+
+//     employee[j]= document.createElement('div');
+//     employee[j].className='employee';
+//     row[i].appendChild(employee[j]);
+//     // employee[j].style.display='flex'
+
+//     if (row[i]==row[numberOfRows-1]){
+//       break;
+//     }
+//   }
+
+// }
+
+
+
+
+  
+// let card=document.getElementsByClassName('employee');
+
+
+// for (let i = 0; i < allEmp.length; i++) {
+//   let picture=document.createElement('img')
+//   picture.src=allEmp[i]['image']
+//   card[0].appendChild(picture);
+//   picture.style.width='250px';
+
+//   let paragraph_01= document.createElement('p')
+//   paragraph_01.textContent='Name: '+allEmp[i]['fullName']+ ' - ID: '+allEmp[i].employeeId()
+//   paragraph_01.className='p1';
+//   card[0].appendChild(paragraph_01)
+
+//   let paragraph_02= document.createElement('p')
+//   paragraph_02.textContent='Department: '+allEmp[i]['department']+ ' - Level: '+allEmp[i]['level']
+//   paragraph_02.className='p2';
+//   card[0].appendChild(paragraph_02)
+
+//   let paragraph_03= document.createElement('p')
+//   paragraph_03.textContent=allEmp[i]['salary']
+//   paragraph_03.className='p3';
+//   card[0].appendChild(paragraph_03)
+// }
+
+
 
 
 
